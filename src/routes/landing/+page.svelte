2c57ddbc4ad970ae5a2345df9407f21349c40abb
rafiharefa <107272666+rafiharefa@users.noreply.github.com>
2025-05-04 23:50:11 +0700

    feat: hero section

---
+++ src/routes/landing/+page.svelte
@@ -0,0 +1,26 @@
+<div class="landing-page">
+    <div class="stickers">
+        <div class="top-left">
+            <img src="/svg/top-left.svg" alt="">
+        </div>
+        <div class="bottom-right">
+            <img src="/svg/bottom-right.svg" alt="">
+        </div>
+        <div class="top-right">
+            <img src="/svg/top-right.svg" alt="">
+        </div>
+        <div class="bottom-left">
+            <img src="/svg/bottom-left.svg" alt="">
+        </div>
+    </div>
+  <h1>
+    <span>Don't</span>
+    <span class="flee">Flee</span>
+    <span>From<br></span>
+    <span>Your</span>
+    <span class="bill">Bill</span>
+  </h1>
+  <div class="btn">
+    <img src="/svg/icon.svg" alt="icon" />
+  </div>
+</div>
