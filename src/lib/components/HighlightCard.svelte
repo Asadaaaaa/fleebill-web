772f429a2e191c0262873f1ca0c2d4addc07b450
rafiharefa <107272666+rafiharefa@users.noreply.github.com>
2025-05-05 23:50:28 +0700

    feat: desktop highlights

--- src/lib/components/HighlightCard.svelte
+++ src/lib/components/HighlightCard.svelte
@@ -1,12 +1,14 @@
 <script>
-    import Spacer from '$lib/components/Spacer.svelte';
+  import Spacer from "$lib/components/Spacer.svelte";
 
-    let {title, description, color, image} = $props()
+  let { title, description, color, image } = $props();
 </script>
 
 <div class="highlight-card" style={`background-color: var(${color[0]})`}>
+  <div class="text-content">
     <h1 style={`color: var(${color[1]})`}>{title}</h1>
     <p>{description}</p>
-    <Spacer size={24} />
-    <img src={image} alt="Highlight Card">
+  </div>
+  <Spacer size={24} />
+  <img src={image} alt="Highlight Card" />
 </div>
