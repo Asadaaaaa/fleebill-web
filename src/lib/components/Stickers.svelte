6e96f61d13b15fdbb84537badde8d27ece9c4b31
rafiharefa <107272666+rafiharefa@users.noreply.github.com>
2025-05-07 10:34:35 +0700

    feat: stickers gsap

--- src/lib/components/Stickers.svelte
+++ src/lib/components/Stickers.svelte
@@ -1,45 +1,5 @@
 <script>
-  const stickers = [
-    {
-      src: "/images/workflow-sticker/s-f-0.svg",
-      alt: "Sticker 1",
-      size: 150,
-    },
-    {
-      src: "/images/workflow-sticker/s-f-1.svg",
-      alt: "Sticker 2",
-      size: 170,
-    },
-    {
-      src: "/images/workflow-sticker/s-f-2.svg",
-      alt: "Sticker 3",
-      size: 130,
-    },
-    {
-      src: "/images/workflow-sticker/s-f-3.svg",
-      alt: "Sticker 4",
-      size: 160,
-    },
-    {
-      src: "/images/workflow-sticker/s-f-0.svg",
-      alt: "Sticker 5",
-      size: 120,
-    },
-    {
-      src: "/images/workflow-sticker/s-f-1.svg",
-      alt: "Sticker 6",
-      size: 180,
-    },
-    {
-      src: "/images/workflow-sticker/s-f-2.svg",
-      alt: "Sticker 7",
-      size: 140,
-    },
-    
-  ];
-
-
-
+  let {stickers, stickers2, stickers3} = $props();
 </script>
 
 
@@ -47,6 +7,22 @@
   {#each stickers as sticker, index}
     <div 
       class="sticker sticker-{index}"
+      style="--sticker-size: {sticker.size}px;"
+    >
+      <img src={sticker.src} alt={sticker.alt} />
+    </div>
+  {/each}
+  {#each stickers2 as sticker, index}
+    <div 
+      class="sticker-sf sticker-{index}"
+      style="--sticker-size: {sticker.size}px;"
+    >
+      <img src={sticker.src} alt={sticker.alt} />
+    </div>
+  {/each}
+  {#each stickers3 as sticker, index}
+    <div 
+      class="sticker-sf-2 sticker-{index}"
       style="--sticker-size: {sticker.size}px;"
     >
       <img src={sticker.src} alt={sticker.alt} />
--- src/lib/components/Workflow.svelte
+++ src/lib/components/Workflow.svelte
@@ -4,28 +4,211 @@
   import Stickers from "./Stickers.svelte";
   import AppButton from "./AppButton.svelte";
   import { gsap } from "gsap";
+  import { onMount } from "svelte";
+
+  const stickers = [
+    {
+      src: "/images/workflow-sticker/s-f-0.svg",
+      alt: "Sticker 1",
+      size: 150,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-1.svg",
+      alt: "Sticker 2",
+      size: 170,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-2.svg",
+      alt: "Sticker 3",
+      size: 130,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-3.svg",
+      alt: "Sticker 4",
+      size: 160,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-0.svg",
+      alt: "Sticker 5",
+      size: 120,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-1.svg",
+      alt: "Sticker 6",
+      size: 180,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-2.svg",
+      alt: "Sticker 7",
+      size: 140,
+    },
+  ];
+
+  const stickers2 = [
+    {
+      src: "/images/workflow-sticker/s-f-4.svg",
+      alt: "Sticker 1",
+      size: 150,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-5.svg",
+      alt: "Sticker 2",
+      size: 170,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-6.svg",
+      alt: "Sticker 3",
+      size: 130,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-7.svg",
+      alt: "Sticker 4",
+      size: 160,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-4.svg",
+      alt: "Sticker 5",
+      size: 120,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-5.svg",
+      alt: "Sticker 6",
+      size: 180,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-6.svg",
+      alt: "Sticker 7",
+      size: 140,
+    },
+  ];
+
+  const stickers3 = [
+    {
+      src: "/images/workflow-sticker/s-f-8.svg",
+      alt: "Sticker 1",
+      size: 150,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-9.svg",
+      alt: "Sticker 2",
+      size: 170,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-10.svg",
+      alt: "Sticker 3",
+      size: 130,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-11.svg",
+      alt: "Sticker 4",
+      size: 160,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-8.svg",
+      alt: "Sticker 5",
+      size: 120,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-9.svg",
+      alt: "Sticker 6",
+      size: 180,
+    },
+    {
+      src: "/images/workflow-sticker/s-f-10.svg",
+      alt: "Sticker 7",
+      size: 140,
+    },
+  ];
 
   let innerWidth = $state(0);
 
-  const handleNext = () => {
-    const stickers = document.querySelectorAll(".sticker");
+  let stickersRef = $state([]);
+  let stickers2Ref = $state([]);
+  let stickers3Ref = $state([]);
+  let activeIndex = $state(0);
 
-    gsap.to(stickers, {
-      duration: .5,
-      stagger: 0.05,
-      x: -(innerWidth * 1.1),
-      ease: "back.in(10)",
+  onMount(() => {
+    stickersRef = document.querySelectorAll(".sticker");
+    stickers2Ref = document.querySelectorAll(".sticker-sf");
+    stickers3Ref = document.querySelectorAll(".sticker-sf-2");
+
+    gsap.set(stickers2Ref, {
+      x: innerWidth * 1.1,
+      immediateRender: true,
     });
+    gsap.set(stickers3Ref, {
+      x: innerWidth * 1.1,
+      immediateRender: true,
+    });
+  });
+
+  const handleNext = () => {
+    if (activeIndex == 2) {
+      return;
+    } else if (activeIndex == 0) {
+      gsap.to(stickersRef, {
+        duration: 0.5,
+        stagger: 0.05,
+        x: -(innerWidth * 1.1),
+        ease: "back.in(10)",
+      });
+
+      gsap.to(stickers2Ref, {
+        duration: 0.5,
+        stagger: 0.05,
+        x: 0,
+        ease: "back.out(10)",
+      });
+    } else if (activeIndex == 1) {
+      gsap.to(stickers2Ref, {
+        duration: 0.5,
+        stagger: 0.05,
+        x: -(innerWidth * 1.1),
+        ease: "back.in(10)",
+      });
+
+      gsap.to(stickers3Ref, {
+        duration: 0.5,
+        stagger: 0.05,
+        x: 0,
+        ease: "back.out(10)",
+      });
+    }
+
+    activeIndex++;
   };
 
   const handleBack = () => {
-    const stickers = document.querySelectorAll(".sticker");
-    gsap.to(stickers, {
-      duration: 1,
-      stagger: 0.05,
-      x: 0,
-      ease: 'power2.inOut',
-    });
+    if (activeIndex == 0) {
+      return;
+    } else if (activeIndex == 1) {
+      gsap.to(stickersRef, {
+        duration: 0.5,
+        stagger: 0.05,
+        x: 0,
+        ease: "power2.inOut",
+      });
+
+      gsap.to(stickers2Ref, {
+        duration: 0.5,
+        stagger: 0.05,
+        x: innerWidth * 1.1,
+      });
+    } else if (activeIndex == 2) {
+      gsap.to(stickers3Ref, {
+        duration: 0.5,
+        stagger: 0.05,
+        x: innerWidth * 1.1,
+      });
+
+      gsap.to(stickers2Ref, {
+        duration: 0.5,
+        stagger: 0.05,
+        x: 0,
+      });
+    }
+
+    activeIndex--;
   };
 </script>
 
@@ -45,5 +228,5 @@
       <h5>Next</h5>
     </AppButton>
   </div>
-  <Stickers />
+  <Stickers stickers={stickers} {stickers2} {stickers3} />
 </div>
